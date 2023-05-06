2f1d9d2dbce294aba040b8fac5c13f31f0505b0b
val-rij <114986910+val-rij@users.noreply.github.com>
2023-05-06 19:20:49 +0000

    my first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,49 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// window.onload();
+// {
+//   const getRandomNumber = () => {
+//     const random = Math.floor(Math.random() * 4);
+//     return random;
+//   };
+//   const excuseGenerator = () => {
+//     const who = ["The dog", "My grandma", "His turtle", "My bird"];
+//     const action = ["ate", "peed", "crushed", "broke"];
+//     const what = ["my homework", "the keys", "the car"];
+//     const when = [
+//       "before the class",
+//       "right on time",
+//       "when I finished",
+//       "during my lunch",
+//       "while I was praying"
+//     ];
+//     return `${who[getRandomNumber()]} ${action[getRandomNumber()]} ${
+//       what[getRandomNumber()]
+//     } ${when[getRandomNumber()]}`;
+//   };
+//   document.getElementById("excuse").innerHTML = excuseGenerator();
+// }
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.getElementById("btn").addEventListener("click", () => {
+    document.getElementById("excuse").innerHTML = excuseGenerator();
+  });
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const getRandomNumber = () => {
+    const random = Math.floor(Math.random() * 4);
+    return random;
+  };
+  const excuseGenerator = () => {
+    const who = ["The dog", "My grandma", "His turtle", "My bird"];
+    const action = ["ate", "peed", "crushed", "broke"];
+    const what = ["my homework", "the keys", "the car"];
+    const when = [
+      "before the class",
+      "right on time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying"
+    ];
+    return `${who[getRandomNumber()]} ${action[getRandomNumber()]} ${
+      what[getRandomNumber()]
+    } ${when[getRandomNumber()]}`;
+  };
 };
